294cc4b276cee266ecef4491d136e382b4dde050
George Wendo Musungu <info@jumuikahub.com>
2025-08-25 23:00:45 +0300

    chore: remove InternLink and Institutions routes for clean production build

--- app/manager/layout.tsx
+++
@@ -1,6 +0,0 @@
-import { redirect } from "next/navigation";
-
-export default function ManagerAlias() {
-  // Preserve old link, but use the clear new location/label:
-  redirect("/vendors/dashboard");
-}
